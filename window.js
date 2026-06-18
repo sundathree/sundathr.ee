5582703d560d2e5b9a2a39011bdc5bea94616af9
Miguel Carvalho <dev@sundathr.ee>
2026-06-18 14:47:25 -0300

    feat: window preset + touch support

--- window.js
+++ window.js
@@ -1,44 +1,59 @@
-dragElement(document.querySelector(".window"));
+let topZ = 0;
+document.querySelectorAll(".window").forEach(el => dragElement(el));
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    elmnt.style.left = (window.innerWidth  / 2 - elmnt.offsetWidth  / 2) + "px";
-    elmnt.style.top  = (window.innerHeight / 2 - elmnt.offsetHeight / 2) + "px";
+    elmnt.addEventListener('mousedown',  () => elmnt.style.zIndex = ++topZ);
+    elmnt.addEventListener('touchstart', () => elmnt.style.zIndex = ++topZ);
 
-    if (elmnt.querySelector(".window-title")) {
-        elmnt.querySelector(".window-title").onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
+    const titleEl = elmnt.querySelector(".window-title") || elmnt;
+    ['mousedown', 'touchstart'].forEach(ev => titleEl.addEventListener(ev, dragMouseDown));
+
+    function getCoords(e) {
+        let x, y;
+
+        if (e.targetTouches && e.targetTouches.length > 0) {
+            x = e.targetTouches[0].clientX;
+            y = e.targetTouches[0].clientY;
+        } else {
+            x = e.clientX;
+            y = e.clientY;
+        }
+        return { x, y };
     }
 
     function dragMouseDown(e) {
-        e = e || window;
         e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+        const {x, y} = getCoords(e);
+        pos3 = x;
+        pos4 = y;
+        document.addEventListener('mouseup',   closeDragElement);
+        document.addEventListener('touchend',  closeDragElement);
+        document.addEventListener('mousemove', elementDrag);
+        document.addEventListener('touchmove', elementDrag);
     }
 
     function elementDrag(e) {
-        e = e || window;
         e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        // TODO: clamp the window position
-        // var newTop  = elmnt.offsetTop  - pos2;
-        // var newLeft = elmnt.offsetLeft - pos1;
-        // elmnt.style.top  = newTop  + "px";
-        // elmnt.style.left = newLeft + "px";
+        const {x, y} = getCoords(e);
+        pos1 = pos3 - x;
+        pos2 = pos4 - y;
+        pos3 = x;
+        pos4 = y;
+        var newTop  = elmnt.offsetTop  - pos2;
+        var newLeft = elmnt.offsetLeft - pos1;
+        newTop  = Math.max(0, Math.min(newTop,  window.innerHeight - elmnt.offsetHeight));
+        newLeft = Math.max(0, Math.min(newLeft, window.innerWidth  - elmnt.offsetWidth));
+        elmnt.style.top  = newTop  + "px";
+        elmnt.style.left = newLeft + "px";
     }
 
     function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
+        document.removeEventListener('mouseup',   closeDragElement);
+        document.removeEventListener('touchend',  closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
+        document.removeEventListener('touchmove', elementDrag);
     }
 }
+
